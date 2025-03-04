a76d98c05610ffc365d7b7e3dc0ffde1867d4132
Malik Furqan Maqbool <111356933+FurqanMaqbool@users.noreply.github.com>
2025-03-04 12:09:41 +0500

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,14 @@
-pip install -r requirments.txt
-python3 manage.py collectstatic
+# pip install -r requirments.txt
+# python3 manage.py collectstatic
+
+
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run migrations (if needed)
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
