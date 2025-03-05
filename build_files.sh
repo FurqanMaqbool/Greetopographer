0b297f5e8ed05d5f11f689d49e796132eac803a5
Malik Furqan Maqbool <111356933+FurqanMaqbool@users.noreply.github.com>
2025-03-05 09:45:41 +0500

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -7,7 +7,7 @@
 # pip3.12 install --no-cache-dir --find-links=https://raw.githubusercontent.com/FurqanMaqbool/Greetopographer/main/ GDAL-3.10.1-cp312-cp312-win_amd64.whl
 
 # Install other dependencies
-pip3.12 install -r requirements.txt
+pip3.12 install -r requirments.txt
 
 # Run migrations (if needed)
 python3.12 manage.py migrate
